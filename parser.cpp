8df575cb80c8ed1d4e1cefd2fa80362d48c07d3c
inzrv <ivan.nzrv@outlook.com>
2024-07-20 15:39:47 +0300

    Parser. All statements

--- parser.cpp
+++ parser.cpp
@@ -9,6 +9,7 @@ Parser::Parser(Lexer& lexer) : lexer_(lexer) {
 
 // program ::= { statement }
 void Parser::Program() {
+    SkipNewLines();
     while (!CheckCurToken(TokenType::EOFT)) {
         Statement();
     }
@@ -25,15 +26,27 @@ void Parser::Program() {
 void Parser::Statement() {
     if (CheckCurToken(TokenType::PRINT)) {
         PrintStatement();
+    } else if (CheckCurToken(TokenType::IF)) {
+        IfStatement();
+    } else if (CheckCurToken(TokenType::WHILE)) {
+        WhileStatement();
+    } else if (CheckCurToken(TokenType::LABEL)) {
+        LabelStatement();
+    } else if (CheckCurToken(TokenType::GOTO)) {
+        GoToStatement();
+    } else if (CheckCurToken(TokenType::LET)) {
+        LetStatement();
+    } else if (CheckCurToken(TokenType::INPUT)) {
+        InputStatement();
+    } else {
+        UnexpectedTokenAbort();
     }
     NewLine();
 }
 
 void Parser::PrintStatement() {
-    std::cout << "Parser::PrintStatement()";
-    curToken_.Print();
+    std::cout << "Parser::PrintStatement()\n";
     NextToken();
-    curToken_.Print();
     if (CheckCurToken(TokenType::STRING)) {
         NextToken();
     } else {
@@ -41,16 +54,75 @@ void Parser::PrintStatement() {
     }
 }
 
+void Parser::IfStatement() {
+    std::cout << "Parser::IfStatement()\n";
+    NextToken();
+    Comparsion();
+    Match(TokenType::THEN);
+    NewLine();
+
+    while(!CheckCurToken(TokenType::ENDIF)) {
+        Statement();
+    }
+
+    // Match(TokenType::ENDIF);  We already checked ENDIF in the while-loop, so maybe just NextToken() ?
+    NextToken();
+}
+
+void Parser::WhileStatement() {
+    std::cout << "Parser::WhileStatement()\n";
+    NextToken();
+    Comparsion();
+    Match(TokenType::REPEAT);
+    NewLine();
+
+    while(!CheckCurToken(TokenType::ENDWHILE)) {
+        Statement();
+    }
+
+    NextToken();
+}
+
+void Parser::LabelStatement() {
+    std::cout << "Parser::LabelStatement()\n";
+    NextToken();
+    Match(TokenType::IDENT);
+}
+
+void Parser::GoToStatement() {
+    std::cout << "Parser::GoToStatement()\n";
+    NextToken();
+    Match(TokenType::IDENT);
+}
+
+void Parser::LetStatement() {
+    std::cout << "Parser::LetStatement()\n";
+    NextToken();
+    Match(TokenType::IDENT);
+    Match(TokenType::EQ);
+    Expression();
+}
+
+void Parser::InputStatement() {
+    std::cout << "Parser::InputStatement()\n";
+    NextToken();
+    Match(TokenType::IDENT);
+}
+
 
 void Parser::Expression() {
+    NextToken();
+}
 
+void Parser::Comparsion() {
+    NextToken();
+    NextToken();
+    NextToken();
 }
 
 void Parser::NewLine() {
     Match(TokenType::NEWLINE);
-    while (CheckCurToken(TokenType::NEWLINE)) {
-        NextToken();
-    }
+    SkipNewLines();
 }
 
 bool Parser::CheckCurToken(TokenType type) const {
@@ -63,18 +135,7 @@ bool Parser::CheckPeekToken(TokenType type) const {
 
 void Parser::Match(TokenType type) {
     if (!CheckCurToken(type)) {
-        std::string tokenTypeName;
-        std::string expectedTypeName;
-
-        const auto& resCur = g_tokenTypeNamesMap.find(curToken_.GetType());
-        const auto& resExpected = g_tokenTypeNamesMap.find(type);
-        if (resCur != g_tokenTypeNamesMap.end()  && resExpected != g_tokenTypeNamesMap.end()) {
-            tokenTypeName = resCur->second;
-            expectedTypeName = resExpected->second;
-        }
-        std::string message = "Error! Token {" + curToken_.GetText() + ":" +
-            tokenTypeName + "} doesn't match with " + expectedTypeName;
-        Abort(message);
+        UnexpectedTokenAbort(type);
     }
     NextToken();
 }
@@ -86,4 +147,30 @@ void Parser::NextToken() {
 
 void Parser::Abort(const std::string& message) const {
     std::cerr << "Parser::Abort: " << message << std::endl;
+    assert(false);
+}
+
+void Parser::UnexpectedTokenAbort(std::optional<TokenType> type) const {
+    std::string tokenTypeName;
+    const auto& resCur = g_tokenTypeNamesMap.find(curToken_.GetType());
+    if (resCur != g_tokenTypeNamesMap.end()) {
+        tokenTypeName = resCur->second;
+    }
+
+    std::string expectedTypeName{"Undefined"};
+    if (type) {
+        const auto& resExpected = g_tokenTypeNamesMap.find(type.value());
+        if (resExpected != g_tokenTypeNamesMap.end()) {
+            expectedTypeName = resExpected->second;
+        }
+    }
+    std::string message = "Parser::UnexpectedTokenAbort! Token {" + curToken_.GetText() + " : " +
+        tokenTypeName + "} doesn't match with { ... : " + expectedTypeName + "}";
+    Abort(message);
+}
+
+void Parser::SkipNewLines() {
+    while (CheckCurToken(TokenType::NEWLINE)) {
+        NextToken();
+    }
 }
--- parser.hpp
+++ parser.hpp
@@ -8,7 +8,14 @@ public:
     void Program();
     void Statement();
     void PrintStatement();
+    void IfStatement();
+    void WhileStatement();
+    void LabelStatement();
+    void GoToStatement();
+    void LetStatement();
+    void InputStatement();
     void Expression();
+    void Comparsion();
     void NewLine();
     bool CheckCurToken(TokenType type) const;
     bool CheckPeekToken(TokenType type) const;
@@ -16,6 +23,9 @@ public:
     void NextToken();
     void Abort(const std::string& message) const;
 private:
+    void SkipNewLines();
+    void UnexpectedTokenAbort(std::optional<TokenType> type = std::nullopt) const;
+
     Lexer& lexer_;
     Token curToken_;
     Token peekToken_;
